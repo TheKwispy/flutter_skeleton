f01771b8cd691b7cc4e2278e268f0eb26041cf56
Kristian Martin <62425499+TheKwispy@users.noreply.github.com>
2021-03-09 10:57:37 +0200

    Create app_colors.dart

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color background = Color(0xFFCAD2CE);
+  static const Color surface = Color(0xFF52796F);
+  static const Color border = Color(0xFF2F3E46);
+  static const Color primary = Color(0xFF354F52);
+  static const Color secondary = Color(0xFF84A98C);
+  static const Color boxShadow = Colors.black;
+}
