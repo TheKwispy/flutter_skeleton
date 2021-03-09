958caa37badea4bfe54f9e96a8dfb61dce9f0e98
Kristian Martin <62425499+TheKwispy@users.noreply.github.com>
2021-03-09 10:57:00 +0200

    Create app_dimens.dart

---
+++ lib/resources/app_dimens.dart
@@ -0,0 +1,21 @@
+  class AppDimens {
+  // These are so that the dimensions are uniform, and we can tweak them in one place
+  static const double XS2 = 2;
+  static const double XS = 4;
+  static const double SMALL = 8;
+  static const double SM = 12;
+  static const double MEDIUM = 16;
+  static const double ML = 20;
+  static const double LARGE = 24;
+  static const double XL = 32;
+  static const double XL2 = 40;
+  static const double XL3 = 48;
+  static const double XL4 = 56;
+  static const double XL5 = 64;
+  static const double XL6 = 72;
+  static const double XL7 = 80;
+  static const double XL8 = 96;
+  static const double BUTTON_HEIGHT = 40;
+  static const double TILE_LEADING_HEIGHT = 48;
+  static const double THUMBNAIL_DEFAULT_SIZE = 100;
+}
